3001bacbb95216f46e06554eae0f2a8f4fd90483
Daniel Mamnev <danielmamnev@gmail.com>
2022-09-29 20:50:53 -0400

    Delete SpectrumSampleApp directory

--- SpectrumSampleApp/src/SpectrumSampleApp.Core/CustomControls/CustomPicker.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Xamarin.Forms;
-
-namespace SpectrumSampleApp.Core.CustomControls
-{
-    public class CustomPicker : Picker
-    {
-    }
-}
--- SpectrumSampleApp/src/SpectrumSampleApp.Core/IAudioService.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace SpectrumSampleApp.Core
-{
-    public interface IAudioService
-    {
-        void PlayAudio(string fileName);
-    }
-}
